b0af4ecf22256fa4bf57eca1cf95347aa1a30220
Kentaro Wuertele <ken@wuertele.com>
2022-11-27 11:10:38 -0800

    Added keyboard and mouse input tracker.

---
+++ c-c++/key-mouse-sdl2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+using namespace std;
+
+int main ()
+{
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window *win = SDL_CreateWindow("title", 30, 15, 600, 500, SDL_WINDOW_SHOWN);
+  SDL_Surface *screen = SDL_GetWindowSurface(win);
+  SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_SOFTWARE);
+
+  // Change SDL drawing color.
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  // Fill the screen with the color.
+  SDL_RenderClear(renderer);
+  // Draw anything that has not been rendered yet. Similar to refreshing the screen.
+  SDL_RenderPresent(renderer);
+  
+  bool run = true;
+  
+  while (run) {
+    SDL_Event ev;
+    
+    while (SDL_PollEvent(&ev)) {
+      switch (ev.type) {
+      case SDL_QUIT:
+	cout << "Quitting\n";
+	run = false;
+	break;
+	// Keyboard
+      case SDL_KEYDOWN:
+	cout << SDL_GetKeyName(ev.key.keysym.sym) << " has been pressed\n";
+	break;
+      case SDL_KEYUP:
+	cout << SDL_GetKeyName(ev.key.keysym.sym) << " has been released\n";
+	break;
+	// Mouse
+      case SDL_MOUSEBUTTONDOWN:
+	cout << SDL_BUTTON(ev.button.button) << " pressed at x: " << ev.button.x << ", y: " << ev.button.y << "\n";
+	break;
+      case SDL_MOUSEBUTTONUP:
+	cout << SDL_BUTTON(ev.button.button) << " released at x: " << ev.button.x << ", y: " << ev.button.y << "\n";
+	break;
+      }
+    }
+  }
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+}
