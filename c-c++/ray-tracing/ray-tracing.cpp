7f0a0a2fb59998eb82c0209695cb2c6f6b971d0f
Kentaro Wuertele <ken@wuertele.com>
2023-09-25 07:59:42 -0700

    Working on new ray tracing project.

--- c-c++/ray-tracing/ray-tracing.cpp
+++ c-c++/ray-tracing/ray-tracing.cpp
@@ -2,19 +2,21 @@
 #include <fstream>
 #include <vector>
 
-void twod_vector_to_ppm (std::vector<std::vector<Ray>> &rays, std::string &file_name) {
-  int width {static_cast<int>(rays.size())};
-  int height {static_cast<int>(rays[0].size())};
+#include "vector3.h"
+
+void twod_vector_to_ppm (std::vector<std::vector<Vector3>> &colors, std::string &file_name) {
+  int width {static_cast<int>(colors.size())};
+  int height {static_cast<int>(colors[0].size())};
   
   std::ofstream my_file;
   my_file.open(file_name);
   
-  my_file << "P3\n" << rays.size() << " " << rays[0].size() << "\n255\n";
+  my_file << "P3\n" << width << " " << height << "\n255\n";
 
   // The top left of the image is [0][0] in the array, and the top right is [width - 1][0].
   for (int y {0}; y < height; y++) {
     for (int x {0}; x < width; x++) {
-      //      my_file << 
+      my_file << colors[x][y].r() << " " << colors[x][y].g() << " " << colors[x][y].b() << "\n";
     }
   }
 
@@ -22,20 +24,18 @@ void twod_vector_to_ppm (std::vector<std::vector<Ray>> &rays, std::string &file_
 }
 
 int main () {
-  /*  int width {1000};
-      int height {1000};*/
+  //int width {1000};
+  //int height {1000};
   int width {256};
   int height {256};
 
-  std::vector<std::vector<Ray>> rays;
-  rays.resize(width);
+  std::vector<std::vector<Vector3>> colors;
+  colors.resize(width);
   
   for (int x {0}; x < width; x++) {
-    rays[x].resize(height);
+    colors[x].resize(height);
     for (int y {0}; y < height; y++) {
-      rays[x][y].red = x;
-      rays[x][y].green = 0;
-      rays[x][y].blue = 0;
+      colors[x][y][0] = 3;
     }
   }
 
