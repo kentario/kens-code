137a05ddb98440e8075a0c476b82d6f0bdc24f94
ken <ken@penguin>
2024-02-18 12:40:30 -0800

    Moved encoding and decoding to seperate functions.

--- c/ende.c
+++ c/ende.c
@@ -1,59 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+/* output_string:  buffer to return encoded content
+   output_alloc:  buffer has this many character allocated (includes space for null termination)
+   input_string:  zero-terminated string to encode
+   offset: the caesar-cypher offset
+*/
+
+int encode (char *output_string, size_t output_alloc, const char *input_string, int offset) {
+  // Does not include space for null termination.
+  size_t input_string_len = strlen(input_string);
+  if (input_string_len + 3 > output_alloc - 1) {
+    printf ("Input string is too large.");
+    return -1;
+  }
+
+  bzero (output_string, output_alloc);
+  
+  sprintf(output_string, "%03d", offset);
+  
+  for (int i = 0; input_string[i]; i++) {
+    unsigned char c = input_string[i] + offset;
+
+    while (c > 127) {
+      c -= 95;
+    }
+
+    output_string[i + 3] = c;
+  }
+  
+  return 0;
+}
+
+/* output_string:  buffer to return decoded content
+   output_alloc:  buffer has this many character allocated (includes space for null termination)
+   input_string:  zero-terminated string to decode
+   offset: the caesar-cypher offset
+*/
+int decode (char *output_string, size_t output_alloc, const char *input_string, int offset) {
+  // Does not include space for null termination.
+  size_t input_string_len = strlen(input_string);
+  if (input_string_len - 3 > output_alloc - 1) {
+    printf ("Input string is too large.");
+    return -1;
+  }
+
+  bzero (output_string, output_alloc);
+  
+  sprintf(output_string, "%03d", offset);
+  
+  for (int i = 3; input_string[i]; i++) {
+    unsigned char c = input_string[i] - offset;
+
+    while (c < 32) {
+      c += 95;
+    }
+
+    output_string[i - 3] = c;
+  }
+  
+  return 0;
+}
 
 int main (int argc, char *argv[]) {
   if (argc != 3) {
     printf("The correct usage is %s <encode or decode> message\n", argv[0]);
     return EXIT_FAILURE;
   }
-
+ 
   // d for decode, e for encode.
   char action = argv[1][0];
 
   int input_length = sizeof(argv[1])/sizeof(argv[1][0]);
   
   // Plus 3 for the offset, plus 1 for null character.
-  char result[input_length + 4];
+  size_t result_alloc = input_length + 4;
+  char result[result_alloc];
 
   int offset;
   
   switch (action) {
   case 'e':
-    offset = argv[2][0];
-
-    sprintf(result, "%03d", offset);
-
-    // i starts at 3 to skip the offset.
-    for (int i = 0; argv[2][i]; i++) {
-      unsigned char c = argv[2][i] + offset;
-
-      // Make sure the character stays below 127.
-      while (c > 127) {
-        c -= 95;
-      }
-      
-      result[i + 3] = c;
-    }
-
-    result[input_length + 4] = '\0';
+    offset = (int) argv[2][0];
+    
+    encode(result, result_alloc, argv[2], offset);
     break;
   case 'd':
     char offset_string[] = {argv[2][0], argv[2][1], argv[2][2], '\0'};
+    
     offset = atoi(offset_string);
 
-    // i starts at 3 to skip the offset.
-    for (int i = 3; argv[2][i]; i++) {
-      unsigned char c = argv[2][i] - offset;
-      
-      // Make sure the character stays above 32.
-      while (c < 32) {
-        c += 95;
-      }
-
-      result[i - 3] = c;
-    }
-
-    result[input_length - 3] = '\0';
+    decode(result, result_alloc, argv[2], offset);
     break;
   default:
     printf("Please use either encode or decode for the second argument.\n");
