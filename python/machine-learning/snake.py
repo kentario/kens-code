7a0a5aeec340d1f958f2457fedb01f15f188b8f0
ken <ken@penguin>
2024-03-31 19:59:08 -0700

    Changed almost all python lists to numpy arrays.

--- python/machine-learning/snake.py
+++ python/machine-learning/snake.py
@@ -1,15 +1,24 @@
 import random
+import numpy
 import pygame
 
-#               up     right    down    left  straight
+# up     right    down    left  straight
 directions = ((0, -1), (1, 0), (0, 1), (-1, 0), None)
+
+# Returns whether numpy array sub is contained in numpy array arr.
+def contains (arr, sub):
+    return numpy.any(numpy.all(arr == sub, axis=1))
+
+# Removes a specific 1d numpy array from 2d numpy array arr.
+def remove (arr, element):
+    return numpy.delete(arr, numpy.where(numpy.all(arr == element, axis=1)), axis=0)
 
 class Snake:
     def __init__ (self):
         self.length = 2
 
-        self.head = [1, 0]
-        self.body = [[0, 0]]
+        self.head = numpy.array([1, 0])
+        self.body = numpy.array([[0, 0]])
 
         # Default direction to the right
         self.direction = directions[1]
@@ -17,25 +26,25 @@ class Snake:
     # None can be used to represent no turn.
     def turn (self, direction):
         # If the components add to 0, then it is a 180 degree turn, which is the same as no turn.
-        if ((direction is None) or
-            (self.direction[0] + direction[0] == 0 and
-             self.direction[1] + direction[1] == 0)):
+        if (direction is None or
+            self.direction[0] + direction[0] == 0 or
+            self.direction[1] + direction[1] == 0):
             return
         
         self.direction = direction
 
     # Returns true if an apple was eaten, false if not.
     def move (self, apples):
-        self.body = [self.head] + self.body
-        self.head = [self.head[0] + self.direction[0], self.head[1] + self.direction[1]]
+        self.body = numpy.vstack([self.head, self.body])
+        self.head += self.direction
 
         # Check if the head is in an apple.
-        if (self.head in apples):
+        if (contains(apples, self.head)):
             self.length += 1
             return True
         else:
             # Only shorten the body if the head is not in an apple.
-            self.body.pop()
+            self.body = self.body[:-1]
             return False
 
 class Game:
@@ -43,14 +52,13 @@ class Game:
         self.width = width
         self.height = height
         self.num_apples = num_apples
-
         # +1 for surviving, +2 for eating an apple.
         self.score = 0
         self.snake = Snake()
-
-        self.apple_locations = []
+        self.apple_locations = numpy.empty((num_apples, 2), dtype=int)
+        
         for i in range(num_apples):
-            self.apple_locations.append(self.new_apple_location())
+            self.apple_locations[i] = self.new_apple_location()
 
     def win (self):
         return self.snake.length >= self.width * self.height
@@ -61,24 +69,20 @@ class Game:
         return ((self.snake.head[0] < 0 or self.snake.head[0] >= self.width or
                  self.snake.head[1] < 0 or self.snake.head[1] >= self.height) or
         # Or if the snake's head is in its body.
-                (self.snake.head in self.snake.body))
+                (contains(self.snake.body, self.snake.head)))
     
     def new_apple_location (self):
-        apple_location = [random.randint(0, self.width - 1),
-                          random.randint(0, self.height - 1)]
-        max = 1000
-        while (apple_location in self.apple_locations or
-               apple_location in [self.snake.head] + self.snake.body):
-            if (max <= 0):
-                # TODO: Handle this better.
-                print("No apple location found")
-                exit()
+        max_tries = 1000
+        for i in range(max_tries):
+            apple_location = numpy.array([random.randint(0, self.width - 1),
+                                          random.randint(0, self.height - 1)])
+
+            if (not contains(self.snake.body, apple_location) and
+                not numpy.array_equal(apple_location, self.snake.head) and
+                not contains(self.apple_locations, apple_location)):
+                return apple_location
             
-            apple_location = [random.randint(0, self.width - 1),
-                              random.randint(0, self.height - 1)]
-            max -= 1
-            
-        return apple_location
+        raise ValueError("No valid apple location found")
 
     # Win loss detection is done from outside this method.
     def update (self, turn):
@@ -91,14 +95,14 @@ class Game:
         # Create new apple if needed.
         if (apple_eaten):
             self.score += 2
+            self.apple_locations = remove(self.apple_locations, self.snake.head)
             # If there is no space for a new apple, don't create a new one.
             if (self.num_apples + self.snake.length > self.width * self.height):
                 self.num_apples -= 1
-                self.apple_locations.remove(self.snake.head)
             else:
-                # The head of the snake will be at the location of the eaten apple.
-                self.apple_locations[self.apple_locations.index(self.snake.head)] = self.new_apple_location()
-    
+                # If there is space for an apple, make one.
+                self.apple_locations = numpy.vstack([self.apple_locations, self.new_apple_location()])
+
 def main ():
     pygame.init()
     
@@ -107,13 +111,14 @@ def main ():
     pixel_size = 20
     surface = pygame.display.set_mode((width * pixel_size, height * pixel_size))
     
-    game = Game(width, height, 15)
+    game = Game(width, height, 20)
     
     clock = pygame.time.Clock()
-    fps = 5
-            
-    running = True
+    fps = 3
+
+    num_epochs = 1000
     
+    running = True
     while (running):
         turn = None
         for event in pygame.event.get():
@@ -122,15 +127,19 @@ def main ():
                 break;
             # Check if the user wants to turn the snake.
             elif (event.type == pygame.KEYDOWN):
-                if (event.key == pygame.K_w or event.key == pygame.K_UP):
+                if (event.key in [pygame.K_w, pygame.K_UP]):
                     turn = directions[0]
-                elif (event.key == pygame.K_a or event.key == pygame.K_LEFT):
+                    break
+                elif (event.key in [pygame.K_a, pygame.K_LEFT]):
                     turn = directions[3]
-                elif (event.key == pygame.K_s or event.key == pygame.K_DOWN):
+                    break
+                elif (event.key in [pygame.K_s, pygame.K_DOWN]):
                     turn = directions[2]
-                elif (event.key == pygame.K_d or event.key == pygame.K_RIGHT):
+                    break
+                elif (event.key in [pygame.K_d, pygame.K_RIGHT]):
                     turn = directions[1]
-        
+                    break
+                    
         game.update(turn)
         if (game.win()):
             print("Win")
@@ -143,16 +152,16 @@ def main ():
 
         surface.fill("black")
         # Draw the snake
-        for s in [game.snake.head] + game.snake.body:
+        for s in numpy.vstack([game.snake.head, game.snake.body]):
             rect = pygame.Rect((s[0] * pixel_size, s[1] * pixel_size), (pixel_size, pixel_size))
             pygame.draw.rect(surface, (0, 255, 0), rect)
         # Draw the apples.
         for a in game.apple_locations:
             rect = pygame.Rect((a[0] * pixel_size, a[1] * pixel_size), (pixel_size, pixel_size))
             pygame.draw.rect(surface, (255, 0, 0), rect)
-            
+                    
         pygame.display.flip()
-        
+                    
         delta = clock.tick(fps)
 
     print(f"The score was: {game.score}")
