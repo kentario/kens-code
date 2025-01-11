70075c5a9447d25b3e876c2252ed0c0c7fa19585
Kentaro Wuertele <ken@wuertele.com>
2025-01-10 22:05:16 -0800

    Switching to SFML 3.0.0.

--- c++/learning/learn-sfml/hello-world.cpp
+++ c++/learning/learn-sfml/hello-world.cpp
@@ -1,31 +1,24 @@
-#include <iostream>
-
 #include <SFML/Graphics.hpp>
 
 int main (int argc, char *argv[]) {
-  sf::RenderWindow window{sf::VideoMode {200, 200}, "Hello World"};
-  sf::Event ev;
+  sf::RenderWindow window{sf::VideoMode {{200, 200}}, "Hello World", sf::Style::Titlebar | sf::Style::Close};
 
+  // Only run the program while the window is open.
   while (window.isOpen()) {
-    while (window.pollEvent(ev)) {
-      switch (ev.type) {
-      case sf::Event::Closed:
+    // Check all triggered events since the last iteration of the loop.
+    while (const std::optional event {window.pollEvent()}) {
+      if (event->is<sf::Event::Closed>()) {
 	window.close();
-	break;
-      case sf::Event::KeyPressed:
-	std::cout << ev.key.code << '\n';
-	break;
       }
     }
 
-    // Update
-
-    // Clear old frame
-    window.clear();
+    // Clear old frame.
+    // Not needed because nothing is being drawn.
+    window.clear(sf::Color::Black);
     
-    // Draw Stuff
+    // Draw Stuff.
     
-    // Render the stuff drawn into the screen.
+    // Display the drawn stuff.
     window.display();
   }
   
