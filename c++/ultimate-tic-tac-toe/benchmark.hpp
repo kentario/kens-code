7e456a4fb085b67075e2b5443edc688754a81216
Kentaro Wuertele <ken@wuertele.com>
2025-12-31 11:04:07 -0800

    storing more info about search.

--- c++/ultimate-tic-tac-toe/benchmark.hpp
+++ c++/ultimate-tic-tac-toe/benchmark.hpp
@@ -5,8 +5,11 @@
 #include <string_view>
 #include <chrono>
 #include <random>
+#include <span>
+#include <algorithm>
 
 #include "game.hpp"
+#include "heuristic.hpp"
 #include "bots.hpp"
 
 Board random_position (std::mt19937 &rng, const bool terminal_allowed, const size_t min_moves = 20, const size_t max_moves = 40) {
@@ -16,13 +19,22 @@ Board random_position (std::mt19937 &rng, const bool terminal_allowed, const siz
   if (min_moves > max_moves) {
     throw std::domain_error {"Minimum number of moves is greater than the maximum"};
   }
+  if (min_moves > 81 || max_moves > 81) {
+    throw std::domain_error {"More than 81 moves cannot be played."};
+  }
   
   std::uniform_int_distribution<size_t> dist {min_moves, max_moves};
-  const size_t target_num_moves {dist(rng)};
+  size_t target_num_moves {dist(rng)};
 
   bool board_found {false};
+  size_t tries {0};
   while (!board_found) {
+    tries++;
     board = Board {};
+
+    if (tries >= 50) {
+      target_num_moves = std::max(min_moves, target_num_moves - 1);
+    }
 
     size_t moves_played {0};
     for (; moves_played < target_num_moves; moves_played++) {
@@ -42,12 +54,10 @@ Board random_position (std::mt19937 &rng, const bool terminal_allowed, const siz
       }
     }
 
-    // If terminal boards are allowed, then the board just has to have reached close enough to the target number of moves.
-    // If terminal boards are not allowed, then the board also can't be terminal.
     if (terminal_allowed) {
-      board_found = (moves_played >= min_moves);
+      board_found = moves_played >= min_moves;
     } else {
-      board_found = (moves_played >= min_moves && !board.terminal());
+      board_found = moves_played >= min_moves && !board.terminal();
     }
   }
 
@@ -57,13 +67,20 @@ Board random_position (std::mt19937 &rng, const bool terminal_allowed, const siz
 struct Benchmark_Result {
   std::string name;
   size_t positions;
+  size_t nodes;
+  size_t cutoffs;
   double ms;
   double ms_per_position;
+  double ms_per_node;
 };
 
 std::ostream& operator<< (std::ostream &os, const Benchmark_Result &res) {
-  os << res.name << " played " << res.positions << " positions,\n";
-  os << "taking " << res.ms << " milliseconds, or " << res.ms_per_position << " ms per position";
+  os << "{\n";
+  os << "    " << res.name << " played " << res.positions << " positions in " << res.ms << " milliseconds\n";
+  os << "    " << res.ms_per_position << " ms per position\n";
+  os << "    searched " << res.nodes << " nodes and pruned " << res.cutoffs << " times\n";
+  os << "    " << res.ms_per_node << " ms per node\n";
+  os << "}";
   return os;
 };
 
@@ -80,16 +97,41 @@ Benchmark_Result benchmark_bot_move_generation (Bot &bot, std::span<Board> posit
   return Benchmark_Result {
     .name = bot.get_name(),
     .positions = positions.size(),
+    .nodes = bot.stats.nodes,
+    .cutoffs = bot.stats.cutoffs,
     .ms = ms,
-    .ms_per_position = ms/positions.size()
+    .ms_per_position = ms/positions.size(),
+    .ms_per_node = ms/bot.stats.nodes
   };
 }
 
-Benchmark_Result benchmark_heuristic (Heuristic h, const std::string &name, std::span<Board> positions) {
+Benchmark_Result benchmark_bot_nodes (Bot &bot, std::span<Board> positions) {
   auto start = std::chrono::steady_clock::now();
 
   for (const auto &p : positions) {
-    volatile int m __attribute__((unused)) {h(p)};
+    volatile Move m __attribute__((unused)) {bot(p)};
+  }
+  
+  auto end = std::chrono::steady_clock::now();
+  double ms {std::chrono::duration<double, std::milli>(end - start).count()};
+
+  return Benchmark_Result {
+    .name = bot.get_name(),
+    .positions = positions.size(),
+    .nodes = bot.stats.nodes,
+    .cutoffs = bot.stats.cutoffs,
+    .ms = ms,
+    .ms_per_position = ms/positions.size(),
+    .ms_per_node = ms/bot.stats.nodes
+  };
+}
+
+template <Heuristic H>
+Benchmark_Result benchmark_heuristic (H h, const std::string &name, std::span<Board> positions) {
+  auto start = std::chrono::steady_clock::now();
+
+  for (const auto &p : positions) {
+    [[maybe_unused]] volatile double m {h(p, Eval_Params {})};
   }
   
   auto end = std::chrono::steady_clock::now();
@@ -98,8 +140,11 @@ Benchmark_Result benchmark_heuristic (Heuristic h, const std::string &name, std:
   return Benchmark_Result {
     .name = name,
     .positions = positions.size(),
+    .nodes = 0,
+    .cutoffs = 0,
     .ms = ms,
-    .ms_per_position = ms/positions.size()
+    .ms_per_position = ms/positions.size(),
+    .ms_per_node = 0
   };
 }
 
@@ -116,7 +161,10 @@ Benchmark_Result benchmark_find_legal_moves (std::span<Board> positions) {
   return Benchmark_Result {
     .name = "is_legal",
     .positions = positions.size(),
+    .nodes = 0,
+    .cutoffs = 0,
     .ms = ms,
-    .ms_per_position = ms/positions.size()
+    .ms_per_position = ms/positions.size(),
+    .ms_per_node = 0
   };
 }
